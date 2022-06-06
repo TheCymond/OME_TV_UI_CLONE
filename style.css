7a7457e03499a9a2cac68e34695b39681f362dce
The Cymond <thecymond@gmail.com>
2022-06-06 13:01:32 +0430

    final

---
+++ style.css
@@ -0,0 +1,141 @@
+@import url("https://fonts.googleapis.com/css2?family=Amaranth:wght@400;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  background-color: #f3f3f3;
+  font-family: "Amaranth";
+  overflow: hidden;
+}
+
+.videos {
+  display: flex;
+  width: 90vw;
+  height: 75vh;
+  margin: 0 auto;
+}
+
+.video-me,
+.video-user {
+  width: 100%;
+  height: 100%;
+}
+
+.videos video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+/* Video Me */
+
+.video-me {
+  position: relative;
+}
+
+.video-me-logo {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 50%);
+}
+
+/* Buttons */
+.buttons-container {
+  position: absolute;
+  display: flex;
+  width: 300px;
+  left: 50%;
+  transform: translate(-100%, -170%);
+}
+
+.googleplay,
+.appstore {
+  width: 100%;
+  object-fit: cover;
+  width: 300px;
+  margin-left: 10px;
+}
+
+/* Controls */
+
+.controls {
+  display: flex;
+  width: 90vw;
+  justify-content: space-between;
+  margin: 0 auto;
+  margin-top: 0.5rem;
+}
+
+.buttons {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.btn {
+  background-color: #ccc;
+  margin: 0 0.1rem;
+  width: 8rem;
+  height: 8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 1.2rem;
+}
+
+.btn-start {
+  background-color: #5ab893;
+  border-bottom: 20px solid #43a980;
+  transition: 0.3s all;
+}
+.btn-start:hover {
+  background-color: #72c9a6;
+}
+
+.btn-start:active,
+.btn-country:active,
+.btn-sex:active {
+  transform: translateY(5px);
+  border-bottom: none;
+}
+
+.btn-stop-disabled {
+  background-color: #f1b29f;
+}
+
+.btn-country,
+.btn-sex {
+  color: #fff;
+  /* border: 5px solid #ccc; */
+  border-bottom: 20px solid #dbdbdb;
+  transition: 0.3s all;
+}
+
+.btn-country:hover,
+.btn-sex:hover {
+  background-color: rgb(178, 177, 177);
+}
+
+/* Chat */
+
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  justify-content: baseline;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.send-chat {
+  justify-content: flex-end;
+}
